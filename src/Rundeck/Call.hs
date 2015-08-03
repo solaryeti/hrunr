8016e0564bf67afd9a9493e07249d19b5b8f564c
Steven Meunier <code@solaryeti.com>
2015-08-03 22:14:45 +0200

    documentation

--- src/Rundeck/Call.hs
+++ src/Rundeck/Call.hs
@@ -1,13 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
+
+-- |
+-- Module      : Rundeck.Call
+-- Copyright   : (c) 2015 Steven Meunier
+--
+-- License     : BSD-style
+-- Maintainer  : steven@solaryeti.com
+-- Stability   : experimental
+-- Portability : GHC
+
 module Rundeck.Call
-       ( apiGet
-       , jobExecutions
-       , executionOutput
-       , ApiCall(..)
-       , RundeckResponse
+       (
+       -- * Types
+       -- ** Data Types
+         ApiCall(..)
        , Conninfo(..)
        , Method(..)
+
+       -- ** Type Aliases
+       , RundeckResponse
+       , Param
        , Params
+
+       -- * Functions
+       , apiGet
+       , jobExecutions
+       , executionOutput
        ) where
 
 import Rundeck.Urls
@@ -17,17 +35,20 @@ import qualified Data.ByteString.Lazy as L
 import           Control.Lens ((.~), (&))
 import           Data.Text (Text)
 
-data Method = Get | Post -- | Put | Delete
-type RundeckResponse = (Response L.ByteString)
+-- | HTTP methods
+data Method = Get | Post
 
+type RundeckResponse = (Response L.ByteString)
 type Param = (Text, [Text])
 type Params = [Param]
 
+-- | Conninfo contains the connection info the make a connection to Rundeck
 data Conninfo = Conninfo
-    { host :: String
-    , port :: String
+    { host :: String   -- ^ The hostname of the Rundeck server
+    , port :: String   -- ^ The port the Rundeck server is listening on
     }
 
+-- | Type representing the possible valid calls that can be made against Rundeck
 data ApiCall = SystemInfo
              | Projects
              | ExportJobs
@@ -48,6 +69,8 @@ paramList [] = param "" .~ []
 paramList [(x, xs)] = param x .~ xs
 paramList ((x, xs):xss) = fmap (param x .~ xs) $ paramList xss
 
+-- | Issue a Get request against the Rundeck API
+-- The api endpoint is determined by the type of the 'ApiCall' made.
 apiGet :: ApiCall -> Conninfo -> Params -> IO (Response L.ByteString)
 apiGet a (Conninfo h p) params = getWith (opts params) $ url h p ++ apiurl a
   where apiurl SystemInfo = systemInfoUrl
@@ -61,5 +84,6 @@ jobExecutions :: Conninfo -> Params -> Method -> Id -> IO (Response L.ByteString
 jobExecutions (Conninfo h p) params Get i = getWith (opts params) $ url h p ++ jobExecutionsUrl i
 jobExecutions (Conninfo h p) params Post i = postWith (opts params) (url h p ++ jobExecutionsUrl i) [partText "loglevel" "INFO"]
 
+-- | Get the execution output for a job from Rundeck
 executionOutput ::Conninfo -> Params -> Id -> IO (Response L.ByteString)
 executionOutput (Conninfo h p) params i = getWith (opts params) $ url h p ++ executionOutputUrl i
